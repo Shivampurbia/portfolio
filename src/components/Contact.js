7375f4af7af0f8136406c7b224bc669a714c435b
Shivampurbia <shivam3265prb@gmail.com>
2022-02-11 20:08:13 +0530

    add mailer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  function mailer(e){
+    e.preventDefault();
+    emailjs.sendForm('service_pb6t28a','template_uz3kqid',e.target,'user_DjMFlTUloSX0TTmGpZaS9')
+    .then(res=>{
+      console.log(res);
+    }).catch(err=>{
+      console.log(err);
+    })
+    
+  }
+
   return (
     <section className='my-28' id='contact'>
       <header className='px-5 text-2xl font-bold pt-10'>
@@ -42,7 +54,7 @@ const Contact = () => {
         </div>
 
         <div className='bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 md:rounded-lg shadow-lg  justify-between w-full  md:w-6/12 '>
-          <form className='flex flex-col space-y-3 m-auto w-full' name='contact' method='post'>
+          <form onSubmit={mailer} className='flex flex-col space-y-3 m-auto w-full' name='contact'>
             <input type='hidden' name='form-name' value='contact' />
  
             <label htmlFor='name'>Name</label>
